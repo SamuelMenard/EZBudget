1d25bafd2b499739b40eeee11f2213078bcfbf24
Samuel Menard <samuel.menard@videotron.ca>
2020-02-02 15:52:10 -0500

    Dashboard progress bar done

--- EZBudget/DataModels/Category.cs
+++ EZBudget/DataModels/Category.cs
@@ -19,11 +19,32 @@ namespace EZBudget.DataModels
         public decimal Amount { get; set; }
         public decimal TotalExpenses { get { return Expenses.Sum(x => x.Amount); } }
         public decimal BudgetLeft { get { return Amount - TotalExpenses; } }
+
+        public string PercentageSpent
+        {
+            get
+            {
+                var percentage = TotalExpenses / Amount;
+                return Math.Floor(percentage * 100).ToString() + "%";
+            }
+        }
         public SolidColorBrush BudgetLeftColor { 
             get { 
                 return (BudgetLeft >= 0) ? new SolidColorBrush((Color)ColorConverter.ConvertFromString("#00e600")) 
                     : new SolidColorBrush((Color)ColorConverter.ConvertFromString("#e63900")); 
             } 
+        }
+
+        public double ProgressBarWidth
+        {
+            get
+            {
+                var percentage = TotalExpenses / Amount;
+                percentage = (percentage > 1) ? new decimal(1) : percentage;
+
+                // 200 is the progress bar max width
+                return Decimal.ToDouble(200 * percentage);
+            }
         }
 
         public List<Expense> Expenses { get; set; }
